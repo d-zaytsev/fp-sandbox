465dbf1998be45a32541c99628825db2d0b95451
dmitriy <st107687@student.spbu.ru>
2024-05-14 16:11:11 +0300

    Add open rec mem example

--- lib/mem.ml
+++ lib/mem.ml
@@ -1,5 +1,6 @@
 open Base
 
+(* мемоизация функцию без рекурсии *)
 let mem (f: 'a -> 'b) x = 
   let table = Hashtbl.Poly.create () in
   match Hashtbl.find table x with
@@ -9,17 +10,29 @@ let mem (f: 'a -> 'b) x =
 let rec fac n = if (n <= 1) then 1 else n * fac(n - 1);;
 let mfac = mem fac;;
 
-(* open rec *)
+(* -- открытая рекурсия -- *)
+ let open_mem f x =
+  let table = Hashtbl.Poly.create () in
+  let rec helper x = match Hashtbl.find table x with
+  | Some res -> res
+  | None -> let res = f helper x in Hashtbl.add_exn table ~key:x ~data:(res); res
+   in
+  helper x;;
+
 let open_fib self = function
  | 0 -> 0
  | 1 -> 1
  | n -> self(n - 2) + self(n - 1);;
 
- let rec fib n = open_fib fib n;;
+ let fib n = open_mem open_fib n;;
 
- let open_mem f x =
-  let table = Hashtbl.Poly.create () in
-  let rec helper x = match Hashtbl.find table x with
-  | Some res -> res
-  | None -> Hashtbl.add_exn table ~key:x ~data:(f helper x); f helper x in
- helper x;;
+let open_fac self = function
+  | 1 -> 1
+  | n -> n * self (n-1);;
+
+  let fac n = open_mem open_fac n;;
+
+(* тесты *)
+
+  let%test _ = fac 5 = 120;;
+  let%test _ = fib 5 = 5;;
